a2d4d0bfe9433276e83846e838c255396e78170d
Pooja <pjpooja940@gmail.com>
2026-08-21 21:59:55 +0530

    Wire up evaluate route in main app

---
+++ backend/main.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+from backend.routes import evaluate
+# ...any other imports/routers you already have
+
+app = FastAPI()
+
+app.include_router(evaluate.router)
+# ...any other app.include_router(...) lines you already have
